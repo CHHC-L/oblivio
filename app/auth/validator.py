b7d0588cab7c7d630d2d8175c7a90b9b3ffdf179
chhc <i2333o@outlook.com>
2025-04-14 06:20:27 -0400

    fix. default val for get config

--- app/auth/validator.py
+++ app/auth/validator.py
@@ -3,7 +3,7 @@ from app.auth.email import get_stored_code
 
 def validate_email_domain(email):
     domain = email.split('@')[-1]
-    allowed_domains = current_app.config.get("WHITELIST_DOMAINS").split(',')
+    allowed_domains = current_app.config.get("WHITELIST_DOMAINS", "").split(',')
     return domain in allowed_domains
 
 def verify_code(email, submitted_code):
