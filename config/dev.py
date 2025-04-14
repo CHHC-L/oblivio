79f152ecca03b90a45da4357198b17dbf9ba417b
chhc <i2333o@outlook.com>
2025-04-14 06:34:32 -0400

    fix. load dev config

--- config/dev.py
+++ config/dev.py
@@ -3,8 +3,9 @@ import os
 class DevConfig:
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret')
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT', 587)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', '1') == '1'
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    WHITELIST_DOMAINS = os.environ.get('WHITELIST_DOMAINS')
